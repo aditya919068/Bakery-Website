add31d1cf67b67490819ce8f3cef3a57b6e289c9
Vikash Chauhan <vikashchauhan51@gmail.com>
2020-03-20 15:37:26 +0530

    added order controller

---
+++ src/App/App/Models/OrderModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Models
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public IEnumerable<ProductModel> Items { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+    }
+}
